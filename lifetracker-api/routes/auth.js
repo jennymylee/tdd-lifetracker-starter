facfe3c516474b67813293a7eecbe5bb2855506c
jennymylee <jennylee038@gmail.com>
2022-07-01 10:26:37 -0700

    made models and routes skeleton

--- lifetracker-api/routes/auth.js
+++ lifetracker-api/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/register", async (req, res, next) => {
   try {
-    const user = await User.register({ ...req.body, isAdmin: false });
+    const user = await User.register({ ...req.body });
     return res.status(201).json({ user });
   } catch (err) {
     next(err);
